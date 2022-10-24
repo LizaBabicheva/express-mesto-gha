722dcaf8bec6001f3163f86702dacdb76c34304e
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-10-24 20:53:42 +0300

    add auth middleware and protect all routes except signin and signup

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const mongoose = require('mongoose');
-
 const routerUsers = require('./routes/users');
-
 const routerCards = require('./routes/cards');
-
 const { errorNotFound } = require('./utils/utils');
-
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -30,11 +24,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(routerUsers);
-
 app.post('/signin', login);
 app.post('/signup', createUser);
 
+app.use(auth);
+
+app.use(routerUsers);
 app.use(routerCards);
 
 app.use('*', (req, res) => {
