90cf61957f5ce1acde8b42d17a45740f4fa68ca4
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-10-26 12:43:36 +0300

    instal celebrate

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const routerUsers = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 // const auth = require('./middlewares/auth');
 
 const {
-  getUsers, getUserById, updateUser, updateAvatar, getUserInfo,
+  getUsers, getUserById, updateUser, updateAvatar, getUserInfo, createUser, login,
 } = require('../controllers/users');
 
 routerUsers.get('/users', getUsers);
@@ -15,6 +16,23 @@ routerUsers.get('/users/me', getUserInfo);
 routerUsers.patch('/users/me', updateUser);
 
 routerUsers.patch('/users/me/avatar', updateAvatar);
+
+routerUsers.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30)
+      .email({ minDomainSegments: 2, tlds: { allow: false } }),
+    password: Joi.string().required().pattern(/^[a-zA-Z0-9]{3,30}$/),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(new RegExp(['^[a-zA-Z0-9]{3,30}$'])), // паттерн
+  }),
+}), createUser);
+routerUsers.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30),
+    password: Joi.string().required(), // email,паттерн,
+  }),
+}), login);
 
 // routerUsers.get('/users', auth, getUsers);
 
