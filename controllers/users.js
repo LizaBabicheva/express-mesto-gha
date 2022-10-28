85d77590fc099fd7c0f8a6723f4e44f02c391831
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-10-28 22:07:41 +0300

    refactor error handlers in users controllers

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 const NotFoundError = require('../errors/not-found-err');
 const BadRequestError = require('../errors/bad-request-err');
-const MONGO_DB_CODE = require('../utils/utils');
+const ConflictError = require('../errors/conflict-err');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -43,12 +43,7 @@ module.exports.createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
-  //
-  // if (!email || !password) {
-  //   next(new BadRequestError('Email или пароль не введен'));
-  //   return;
-  // }
-  //
+
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
@@ -59,18 +54,16 @@ module.exports.createUser = (req, res, next) => {
       res.send(userWithoutPassword);
     })
     .catch((err) => {
-      // console.log(err);
-      if (err.code === MONGO_DB_CODE) {
-        res.status(409).send({ message: 'Юзер уже создан' });
+      if (err.code === 11000) {
+        next(new ConflictError(`Пользователь с email '${email}' уже существует`));
+        return;
       }
       if (err.name === 'ValidationError') {
-        next(new BadRequestError('Переданы некорректные данные пользователя'));
-        // res.status(400).send({ message: 'Переданы некорректные данные пользователя' });
-        // return;
+        next(new BadRequestError(err.message));
+        return;
       }
       next(err);
     });
-  // res.status(errorInternal).send({ message: 'Ошибка по-умолчанию' });
 };
 
 module.exports.updateUser = (req, res, next) => {
@@ -83,13 +76,9 @@ module.exports.updateUser = (req, res, next) => {
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        // res.status(errorBadRequest).send({ message:
-        // 'Переданы некорректные данные пользователя' });
-        // return;
-        next(new BadRequestError('Переданы некорректные данные пользователя'));
+        next(new BadRequestError(err.message));
         return;
       }
-      // res.status(errorInternal).send({ message: 'Ошибка по-умолчанию' });
       next(err);
     });
 };
@@ -101,7 +90,7 @@ module.exports.updateAvatar = (req, res, next) => {
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new BadRequestError('Переданы некорректные данные пользователя'));
+        next(new BadRequestError(err.message));
         return;
       }
       next(err);
@@ -110,23 +99,13 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  //
-  if (!email || !password) {
-    return res.status(400).send({ message: 'Не заполнен пароль/мейл' });
-  }
-  //
+
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      // res.send({ token });
       res.status(200)
         .cookie('authorization', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
         .send({ message: 'Успешная авторизация' });
     })
-    // .catch((err) => {
-    //   res.status(401).send({ message: err.message });
-    // });
     .catch(next);
 };
-
-// return next(new MyError(''));
