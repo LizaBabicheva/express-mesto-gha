a8bb4017f86d2c46bea368263cc18b3244549f06
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-10-29 18:39:56 +0300

    add link reg exp in user Schema

--- routes/cards.js
+++ routes/cards.js
@@ -9,8 +9,8 @@ routerCards.get('/cards', getCards);
 
 routerCards.post('/cards', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required.min(2).max(30),
-    link: Joi.string().required,
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/),
   }),
 }), createCard);
 
